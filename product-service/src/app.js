b9d162e4692c70d35447cb472438e7f4f18744d6
unknown <ay22082000@gmail.com>
2024-08-25 15:49:06 +0530

    push

--- product-service/src/app.js
+++ product-service/src/app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-app.use(authorize); // This will protect all routes
+app.use(authorize); 
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
